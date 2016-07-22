17204b79ca56b65aa3e4cfb20e7ef4555c2e8592
Alec Thomas <alec@swapoff.org>
2016-07-22 21:52:40 +1000

    0.9.1 to fix missing Error import.
    Fixes #187.

--- voluptuous/__init__.py
+++ voluptuous/__init__.py
@@ -4,10 +4,12 @@ try:
     from schema_builder import *
     from validators import *
     from util import *
+    from error import *
 except ImportError:
     from .schema_builder import *
     from .validators import *
     from .util import *
+    from .error import *
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __author__ = 'tusharmakkar08'
