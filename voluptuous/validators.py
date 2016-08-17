e92c865d03c326717990537d0d9984773b3cb75b
Dan Girellini <dan@longhands.org>
2016-08-17 15:41:28 -0700

    Invert Range tests so nan is excluded

--- voluptuous/validators.py
+++ voluptuous/validators.py
@@ -475,19 +475,19 @@ class Range(object):
 
     def __call__(self, v):
         if self.min_included:
-            if self.min is not None and v < self.min:
+            if self.min is not None and not v >= self.min:
                 raise RangeInvalid(
                     self.msg or 'value must be at least %s' % self.min)
         else:
-            if self.min is not None and v <= self.min:
+            if self.min is not None and not v > self.min:
                 raise RangeInvalid(
                     self.msg or 'value must be higher than %s' % self.min)
         if self.max_included:
-            if self.max is not None and v > self.max:
+            if self.max is not None and not v <= self.max:
                 raise RangeInvalid(
                     self.msg or 'value must be at most %s' % self.max)
         else:
-            if self.max is not None and v >= self.max:
+            if self.max is not None and not v < self.max:
                 raise RangeInvalid(
                     self.msg or 'value must be lower than %s' % self.max)
         return v
