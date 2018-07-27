7bcc56cbfff29fe3c10268c99b4b9c598f74fc4d
Alec Thomas <alec@swapoff.org>
2018-07-27 19:01:31 +1000

    0.11.3

--- setup.py
+++ setup.py
@@ -9,16 +9,11 @@ import atexit
 sys.path.insert(0, '.')
 version = __import__('voluptuous').__version__
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-    with open('README.rst', 'wb') as f:
-        f.write(long_description.encode('utf-8'))
-    atexit.register(lambda: os.unlink('README.rst'))
-except (ImportError, OSError):
-    print('WARNING: Could not locate pandoc, using Markdown long_description.')
-    with open('README.md') as f:
-        long_description = f.read()
+import pypandoc
+long_description = pypandoc.convert('README.md', 'rst')
+with open('README.rst', 'wb') as f:
+    f.write(long_description.encode('utf-8'))
+atexit.register(lambda: os.unlink('README.rst'))
 
 description = long_description.splitlines()[0].strip()
 
