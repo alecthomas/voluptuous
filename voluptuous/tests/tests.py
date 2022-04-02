7906841a3af8c4357e953664204cf281d208f442
Philip Allgaier <mail@spacegaier.de>
2022-04-02 22:33:47 +0200

    Lint

--- voluptuous/tests/tests.py
+++ voluptuous/tests/tests.py
@@ -91,7 +91,7 @@ def test_in_data_type_container():
         if sys.version_info.major >= 3:
             assert_equal(str(e), "value must be one of [<class 'float'>, <class 'int'>, <class 'str'>] for dictionary value @ data['type']")
         else:
-            assert_equal(str(e), "value must be one of [<type 'float'>, <type 'int'>, <type 'str'>] for dictionary value @ data['type']")    
+            assert_equal(str(e), "value must be one of [<type 'float'>, <type 'int'>, <type 'str'>] for dictionary value @ data['type']")
     else:
         assert False, "Did not raise InInvalid"
 
