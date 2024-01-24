ff820395b187eaecb56f43aade39b14adbf9fba4
Rayyan Baig <154802168+Rayyan925@users.noreply.github.com>
2024-01-24 11:37:22 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.12'
     ]
 )
