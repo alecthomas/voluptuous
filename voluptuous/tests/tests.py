alecthomas/voluptuous

commit 41bc53df12c078a2fc9ea586a280605c53db5ea1

Allow error reporting on keys (#479)

voluptuous/schema_builder.py
@@ -372,7 +372,6 @@ class Schema(object):
                     # key, insert it.
                     key_value_map[key.schema] = key.default()
 
-            error = None
             errors = []
             for key, value in key_value_map.items():
                 key_path = path + [key]
@@ -383,6 +382,7 @@ class Schema(object):
 
                 # compare each given key/value against all compiled key/values
                 # schema key, (compiled key, compiled value)
+                error = None
                 for skey, (ckey, cvalue) in relevant_candidates:
                     try:
                         new_key = ckey(key_path, key)
@@ -430,7 +430,9 @@ class Schema(object):
 
                     break
                 else:
-                    if remove_key:
+                    if error:
+                        errors.append(error)
+                    elif remove_key:
                         # remove key
                         continue
                     elif self.extra == ALLOW_EXTRA:

voluptuous/tests/tests.py
@@ -1600,6 +1600,49 @@ def test_any_with_discriminant():
         assert False, "Did not raise correct Invalid"
 
 
+def test_key1():
+    def as_int(a):
+        return int(a)
+
+    schema = Schema({as_int: str})
+    try:
+        schema({
+            '1': 'one',
+            'two': '2',
+            '3': 'three',
+            'four': '4',
+        })
+    except MultipleInvalid as e:
+        assert len(e.errors) == 2
+        assert str(e.errors[0]) == "not a valid value @ data['two']"
+        assert str(e.errors[1]) == "not a valid value @ data['four']"
+    else:
+        assert False, "Did not raise correct Invalid"
+
+
+def test_key2():
+    def as_int(a):
+        try:
+            return int(a)
+        except ValueError:
+            raise Invalid('expecting a number')
+
+    schema = Schema({as_int: str})
+    try:
+        schema({
+            '1': 'one',
+            'two': '2',
+            '3': 'three',
+            'four': '4',
+        })
+    except MultipleInvalid as e:
+        assert len(e.errors) == 2
+        assert str(e.errors[0]) == "expecting a number @ data['two']"
+        assert str(e.errors[1]) == "expecting a number @ data['four']"
+    else:
+        assert False, "Did not raise correct Invalid"
+
+
 if Enum:
     def test_coerce_enum():
         """Test Coerce Enum"""

commit ca4006a6838be83f682c594bc15f7ea3d79cc80f

Fix vol.Remove not removing keys that do not validate (#515)
* Fix vol.Remove not removing keys

fixes a regression from #479
blocks https://github.com/alecthomas/voluptuous/pull/479

* add test

voluptuous/schema_builder.py
@@ -435,11 +435,11 @@ class Schema(object):
 
                     break
                 else:
-                    if error:
-                        errors.append(error)
-                    elif remove_key:
+                    if remove_key:
                         # remove key
                         continue
+                    elif error:
+                        errors.append(error)
                     elif self.extra == ALLOW_EXTRA:
                         out[key] = value
                     elif self.extra != REMOVE_EXTRA:

voluptuous/tests/tests.py
@@ -176,6 +176,29 @@ def test_remove():
     assert out_ == [1, 2, 1.0, 4]
 
 
+def test_remove_with_error():
+    def starts_with_dot(key: str) -> str:
+        """Check if key starts with dot."""
+        if not key.startswith("."):
+            raise Invalid("Key does not start with .")
+        return key
+
+    def does_not_start_with_dot(key: str) -> str:
+        """Check if key does not start with dot."""
+        if key.startswith("."):
+            raise Invalid("Key starts with .")
+        return key
+
+    schema = Schema(
+        {
+            Remove(All(str, starts_with_dot)): object,
+            does_not_start_with_dot: Any(None),
+        }
+    )
+    out_ = schema({".remove": None, "ok": None})
+    assert ".remove" not in out_ and "ok" in out_
+
+
 def test_extra_empty_errors():
     schema = Schema({'a': {Extra: object}}, required=True)
     schema({'a': {}})